3e33ae88b682adea5f7637aa4c12d62058461f52
vivaca86 <vivaca86@gmail.com>
2026-02-23 07:40:24 +0900

    Fix broken runtime cleanup script and bump to v1.0.12

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'isa-rich-cache-';
-const CACHE_VERSION = 'v9';
+const CACHE_VERSION = 'v10';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const OFFLINE_URLS = [
   './',
